6c1bc3d8073b0a1c30993a0039c27c57831e2669
Matthew Ashmore <54996731+CircuitDev192@users.noreply.github.com>
2021-01-07 19:05:19 -0600

    Katana Implemented
    Does instant max damage on contact. Don't have the need for anything more complicated right now. Can also be thrown at zombies to kill as well.

---
+++ Assets/Scripts/VR Related/MeleeWeapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeapon : MonoBehaviour
+{
+    [SerializeField] private Collider bladeCollider;
+    [SerializeField] private float baseDamage;
+    [SerializeField] private Rigidbody rb;
+    private float impactForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!bladeCollider)
+        {
+            Debug.LogError("Melee Weapon Blade Collider Missing.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        impactForce = rb.velocity.magnitude;
+
+        IDamageAble damageAble = col.gameObject.GetComponentInParent<IDamageAble>();
+
+        if (damageAble != null)
+        {
+            damageAble.Damage(baseDamage, col.gameObject.name);
+            col.GetComponent<Rigidbody>().AddForce((bladeCollider.ClosestPointOnBounds(col.transform.position) - col.transform.position) * impactForce, ForceMode.Impulse);
+        }
+    }
+}
