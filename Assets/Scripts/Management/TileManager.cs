c0908612fa9ed4beb43237afb63496011fd89c4f
Matthew Ashmore <54996731+CircuitDev192@users.noreply.github.com>
2021-04-03 19:13:16 -0500

    The Eye Candy Update
    Added volumetric lighting, clouds, rain, puddles, the ocean, fog, etc.

---
+++ Assets/Scripts/Management/TileManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] tiles;
+    [SerializeField] private float activeDistance;
+    private Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = PlayerManager.instance.player.transform;
+    }
+
+    void FixedUpdate()
+    {
+        foreach (GameObject tile in tiles)
+        {
+            float distance = Vector3.Distance(playerTransform.position, tile.transform.position);
+
+            if (distance > activeDistance) tile.SetActive(false);
+
+            else tile.SetActive(true);
+        }
+    }
+}
